5d7dd553f3afe993e1e0799147eb91bd38ef5612
galirkil <galirkil@gmail.com>
2023-10-12 21:12:12 +0300

    Add MenuItem model

--- menu_maker/admin.py
+++ menu_maker/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Menu
+from .models import Menu, MenuItem
 
 
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
     fields = ('title', 'slug')
     list_display = ('title', 'slug')
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    fields = ('title', 'menu', 'path', 'parent_item')
+    list_display = ('title', 'menu', 'path', 'parent_item')
--- menu_maker/models.py
+++ menu_maker/models.py
@@ -2,21 +2,19 @@ from django.db import models
 
 
 class Menu(models.Model):
-
     title = models.CharField(
         'Название',
         max_length=256,
         help_text='Введите название для меню',
     )
-
     slug = models.SlugField(
         'Человекопонятный идентификатор',
         max_length=256,
         unique=True,
         help_text=(
-            "Укажите уникальный идентификатор для меню. "
-            "Значение этого поля используется для загрузки меню в шаблон. "
-            "Допустимы латинские буквы, цифры и нижнее подчеркивание"
+            'Укажите уникальный идентификатор для меню. '
+            'Значение этого поля используется для загрузки меню в шаблон. '
+            'Допустимы латинские буквы, цифры и нижнее подчеркивание'
 
         )
     )
@@ -29,3 +27,43 @@ class Menu(models.Model):
         verbose_name_plural = 'Меню'
 
 
+class MenuItem(models.Model):
+    title = models.CharField(
+        'Название',
+        max_length=256,
+        help_text='Введите название для пункта меню',
+    )
+    menu = models.ForeignKey(
+        Menu,
+        on_delete=models.CASCADE,
+        related_name='menu_items',
+        verbose_name='Меню',
+        help_text='Выберите меню в которое будет добавлен данный пункт'
+    )
+    path = models.CharField(
+        'Адрес пункта меню',
+        max_length=256,
+        help_text=(
+            'Введите адрес пункта меню. '
+            'Формат: namespace:name'
+        )
+    )
+    parent_item = models.ForeignKey(
+        'self',
+        default=None,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='children_items',
+        verbose_name='Родительский пункт меню',
+        help_text=(
+            'Выберите родительский пункт меню. '
+            'Родительский пункт должен относиться к выбранному меню.  '
+            'Значение по умолчанию создаст '
+            'корневой пункт меню.'
+        )
+    )
+
+    class Meta:
+        verbose_name = 'Пункт меню'
+        verbose_name_plural = 'Пункты меню'
