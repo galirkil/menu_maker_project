32e2f94d1e03028d96c8d4961e55d78be3e3a419
galirkil <galirkil@gmail.com>
2023-10-19 01:59:07 +0300

    Optimize work with database

--- menu_maker/templatetags/menu_maker_tag.py
+++ menu_maker/templatetags/menu_maker_tag.py
@@ -10,23 +10,47 @@ register = template.Library()
 
 @register.inclusion_tag('menu_maker/menu_sample.html', takes_context=True)
 def draw_menu(context: Context, slug: str) -> Dict[str, Any]:
-    all_items = MenuItem.objects.filter(menu__slug=slug)
-    items = all_items.filter(parent_item=None)
+    all_items = MenuItem.objects.filter(menu__slug=slug).select_related(
+        'parent_item')
     path = context['request'].resolver_match.view_name
-    upper_levels, upper_active_items = [], []
+    current_item, current_item_children_items = None, None
+    menu_list, upper_levels, upper_active_items = [], [], []
 
-    if all_items.filter(path=path).exists():
-        current_item = all_items.get(path=path)
-        current_item_children_items = current_item.children_items.all()
+    for i in all_items:
+        menu_list.append(
+            {
+                'id': i.id,
+                'title': i.title,
+                'path': i.path,
+                'parent_item': i.parent_item.id if i.parent_item else None,
+                'children': [],
+            }
+        )
 
-        temp_item = current_item
-        while temp_item.parent_item:
-            upper_active_items.append(temp_item.parent_item)
-            upper_levels.append(temp_item.parent_item.children_items.all())
-            temp_item = temp_item.parent_item
+    for i in menu_list:
+        for j in menu_list:
+            if j['parent_item'] == i['id']:
+                i['children'].append(j)
 
-    else:
-        current_item, current_item_children_items = None, None
+    items = [i for i in menu_list if i['parent_item'] is None]
+
+    for i in menu_list:
+        if i['path'] == path:
+            current_item = i
+
+    for i in menu_list:
+        if i['id'] == current_item['id']:
+            current_item_children_items = i['children']
+
+    temp_item = current_item
+    while temp_item and temp_item['parent_item']:
+        upper_active_items.append(temp_item['parent_item'])
+        for i in menu_list:
+            if i['id'] == temp_item['parent_item']:
+                upper_levels.append(i['children'])
+        for i in menu_list:
+            if i['id'] == temp_item['parent_item']:
+                temp_item = i
 
     menu_dict = {
         'items': items,
